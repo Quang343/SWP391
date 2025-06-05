459a4410482f3b2a9a2959b275bfbed8f2c87eb5
Hoang175 <hoangdhhe181245@fpt.edu.vn>
2025-06-05 22:55:08 +0700

    Update font-end Blog

--- AgriculturalWarehouseManagement/src/main/java/com/example/AgriculturalWarehouseManagement/Fronend/FrontEndController.java
+++ AgriculturalWarehouseManagement/src/main/java/com/example/AgriculturalWarehouseManagement/Fronend/FrontEndController.java
@@ -10,25 +10,45 @@ public class FrontEndController {
     public String home() {
         return "FrontEnd/Home/home";
     }
+
     @RequestMapping("/shop")
     public String shop() {
         return "FrontEnd/Home/shop";
     }
+
     @RequestMapping("/shop/Home/productdetail")
     public String productdetail() {
         return "FrontEnd/Home/productdetail";
     }
+
     @RequestMapping("/login")
     public String login() {
         return "FrontEnd/Home/login";
     }
+
     @RequestMapping("/seller")
     public String seller() {
         return "FrontEnd/Seller/seller";
     }
+
     @RequestMapping("/cart")
     public String cart() {
         return "FrontEnd/Home/cart";
     }
+
+    @RequestMapping("/blog-list")
+    public String bloglist() {
+        return "FrontEnd/Home/blog-list";
+    }
+   @RequestMapping("/blog-detail")
+    public String blogdetail(){
+        return "FrontEnd/Home/blog-detail";
+    }
+
+    @RequestMapping("/blog-grid")
+    public String bloggrid(){
+        return "FrontEnd/Home/blog-grid" ;
+    }
+
 
 }
